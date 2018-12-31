36398369cc44d0dcd5f32d573520a34c1da674f8
youken9980 <youken9980@163.com>
2018-12-31 21:29:37 +0800

    初始化窗口大小

--- simpleCamTest.py
+++ simpleCamTest.py
@@ -9,15 +9,23 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
- 
+# set Width
+cap.set(3,640)
+# set Height
+cap.set(4,480)
+
 while(True):
     ret, frame = cap.read()
-    frame = cv2.flip(frame, -1)
+    # Flip camera vertically
+    frame = cv2.flip(frame, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     cv2.imshow('frame', frame)
     cv2.imshow('gray', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    k = cv2.waitKey(30) & 0xff
+    # press 'ESC' to quit
+    if k == 27:
         break
 
 cap.release()
